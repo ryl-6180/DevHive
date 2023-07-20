affd62b02178d3cb47e6e81354267af9668e2677
sa-arao <arosts1749@gmail.com>
2023-07-20 18:37:03 +0900

    fix : フォームに色々追加

--- js/price_check.js
+++ js/price_check.js
@@ -3,6 +3,9 @@ const formDiv = document.createElement("div");
 formDiv.style.position = "fixed";
 formDiv.style.top = "20px";
 formDiv.style.right = "20px";
+formDiv.style.backgroundColor = "#fff";
+formDiv.style.border = "2px solid #f00";
+formDiv.style.padding = "0.5rem";
 
 const appLabel = document.createElement("label");
 appLabel.value = "価格チェック";
@@ -23,7 +26,18 @@ executeButton.addEventListener("click", startPriceCheck);
 executeButton.style.fontSize = "1rem";
 formDiv.appendChild(executeButton);
 
+const progressCounter = document.createElement("p");
+progressCounter.innerHTML = "進捗（0 / 0）";
+formDiv.appendChild(progressCounter);
+
 document.body.appendChild(formDiv);
+
+// エンターキーで実行できるようにする
+document.getElementById("shopCodeInput").addEventListener("keydown", function (event) {
+	if (event.key === "Enter") {
+		startPriceCheck();
+	}
+});
 
 // アプリケーションIDを指定
 const applicationId = "1008693261382501115";
@@ -73,18 +87,20 @@ function addPriceToLink(apiEndpoint, link, keyword) {
 // リクエストを1秒ずつ間隔を空けて実行する関数
 function executeRequestsSequentially(shopCode, links, currentIndex) {
 	if (currentIndex >= links.length) {
+		progressCounter.innerHTML = "進捗（" + (currentIndex + 1) + " / " + (links.length + 1) + "）";
+		console.log("おわった");
 		alert("オワリマシタ");
 		return; // リンクの全ての要素を処理したら終了
 	}
 
 	let link = links[currentIndex];
-	console.log(link);
 	let url = link.href.split("/");
 	url.pop();
 	let apiEndpoint = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?format=json&keyword=" + url.pop() + "&shopCode=" + shopCode + "&applicationId=" + applicationId;
 	// 商品価格を取得してaタグに追加する関数を呼び出す
 	addPriceToLink(apiEndpoint, link, link.href.split("/").pop());
 
+	progressCounter.innerHTML = "進捗（" + (currentIndex + 1) + " / " + (links.length + 1) + "）";
 	// 次のリクエストを1秒後に実行する
 	setTimeout(function () {
 		executeRequestsSequentially(shopCode, links, currentIndex + 1);
@@ -93,6 +109,10 @@ function executeRequestsSequentially(shopCode, links, currentIndex) {
 // 実行ボタンをクリックしたときに処理を開始する関数
 function startPriceCheck() {
 	const inputElement = document.getElementById("shopCodeInput");
+	if (inputElement.value == "") {
+		alert("ショップコードを入力してください！");
+		return;
+	}
 	const shopCode = inputElement.value;
 	// ページ内のすべてのaタグを取得
 	let allLinks = document.getElementsByTagName("a");
